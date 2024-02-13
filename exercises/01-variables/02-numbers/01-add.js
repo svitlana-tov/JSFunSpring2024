5c5ee8128f8ec0b5f5eca027d1e1682f97e82f6e
Matina Patsos <matinaspatsos@gmail.com>
2024-02-13 16:42:41 -0500

    Revert "Numbers exercises" (#2)
    This reverts commit c03e70b24e2e8240bfcf89c4d0ce1e8258d8402f.

--- exercises/01-variables/02-numbers/01-add.js
+++
@@ -1,6 +0,0 @@
-/**
- * Create a constant called "sum".
- * On the right side of the "equals" sign, there should two numbers and a plus sign.
- */
-
-// WRITE YOUR ANSWER BELOW THIS LINE
--- exercises/01-variables/02-numbers/06-order-of-operations.js
+++
@@ -1,8 +0,0 @@
-/**
- * Without changing the numbers themselves,
- * change the syntax or order of operations so that result equals 800.
- */
-
-// WRITE YOUR ANSWER BELOW THIS LINE
-
-const result = 3 + 5 * 100;
