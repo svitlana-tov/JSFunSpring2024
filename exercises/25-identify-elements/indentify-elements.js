996e5db3e324f47a5105f9f240947a7f8e6eb03f
matinaspatsos <matinaspatsos@gmail.com>
2024-04-21 12:46:30 -0400

    Identify elements exercise

---
+++ exercises/25-identify-elements/indentify-elements.js
@@ -0,0 +1,17 @@
+(function () {
+  /**
+   * When the user clicks on one of the "Select" buttons to select a plan,
+   * show a thank message with the plan's name inside of the aqua green notification at the top of the screen.
+   *
+   * For example, if the user chooses "Premium", display:
+   * "Thank you for purchasing the Premium plan!"
+   *
+   * You must use "document.querySelectorAll" to solve this problem.
+   *
+   * You can edit the HTML on the page to solve this problem.
+   *
+   * As a bonus, you can hide the notification before the user selects a plan.
+   * You can also make the "x" icon dismiss the notification.
+   */
+  // Write your answer here
+})();
