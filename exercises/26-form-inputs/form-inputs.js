e5e30ebf36c318085d65e896fc706a030d87dc8b
Matina Patsos <matinaspatsos@gmail.com>
2024-04-22 20:20:30 -0400

    Form inputs exercises (#24)
    * Forms exercise initial commit
    
    * Form inputs exercises

---
+++ exercises/26-form-inputs/form-inputs.js
@@ -0,0 +1,38 @@
+(function () {
+  /**
+   * You have a few problems to solve below with Vanilla JavaScript.
+   * You are allowed to make changes to the HTML and CSS.
+   */
+  /**
+   * Problem 1: Display what the user is typing or selecting from a dropdown.
+   *
+   * When the user types in an email address,
+   * display what they are typing inside of the empty <span> tags.
+   * When the user selects an option from the dropdown,
+   * display what they selected inside of the empty <span> tags.
+   * (See the comments in the HTML for what <span> tags to sue.)
+   */
+  // Write your answer here
+  /**
+   * Problem 2: Handle a checkbox
+   *
+   * A <div> that says "✓ Include promotional offers" is hidden o the screen.
+   * When the user checks the "Send promotional offers to my email address" checkbox,
+   * show the hidden <div> tag.
+   *
+   * As a bonus, see if you can hide the <div> tag if they uncheck the checkbox.
+   */
+  // Write your answer here.
+  /**
+   * Problem 3: Input validation
+   *
+   * There is a hidden error message "Please enter email address."
+   * If the user has not typed in anything in the email address textbox,
+   * show this error message.
+   * If they have typed something, hide this error message.
+   *
+   * You can use the "input" or "change" events, but the general UX recommendation
+   * is to use "blur" events for these types of error messages (form validation errors).
+   */
+  // Write your answer here
+})();
