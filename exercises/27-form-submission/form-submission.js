e386e7528b281a0ddafc7c84f3971437f1d863fd
Matina Patsos <matinaspatsos@gmail.com>
2024-04-23 07:57:51 -0400

    Form submission exercises (#25)
    * Forms exercise initial commit
    
    * Form inputs exercises
    
    * Form submission exercises
    
    * Correct numbering

---
+++ exercises/27-form-submission/form-submission.js
@@ -0,0 +1,38 @@
+(function () {
+  /**
+   * You have a few problems to solve below with Vanilla JavaScript.
+   * You are allowed to make changes to the HTML and CSS.
+   */
+  // Write your answer here
+  /**
+   * Problem 1: Display the results of the world's most pointless search engine.
+   *
+   * When the user types in the textbook and either clicks "Search" button or hits the enter key,
+   * display the message "No results for ___ found" inside of this <p></p> below.
+   * For example, if the user searches for "Indian Ocean", display "No results for Indian Ocean found".
+   * (Since there are no oceans near Albany, NY, the search engine should
+   * display the "No results for ___ found" message every time.)
+   *
+   * The exercise must be completed with a form handler
+   * and you must prevent the page from refreshing when the form is submitted.
+   */
+  // Write your answer here
+  /**
+   * Problem 2: Agree to the terms and conditions
+   *
+   * Whenever the user clicks the "Continue" button, if she has not agreed to the terms,
+   * the error "You must agree to the terms and conditions" should appear
+   * and the label "I Agree to the Terms and Conditions" should turn red.
+   * If she has, then display "Thank you for signing up".
+   *
+   * To see an example of what this looks like:
+   * When the page loads, @see terms-on-load.png
+   * When the user clicks on the "Continue" button without checking the "I Agree ..." checkbox,
+   * @see terms-on-error.png
+   * When the user checks the "I Agree ..." checkbox and click "Continue",
+   * @see terms-on-success.png
+   *
+   * To start, you will need to hide some element on the page and change the input's classes.
+   */
+  // Write your answer here
+})();
